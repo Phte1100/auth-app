a1d3047fdce5196abfdd05fbe474e341b3b66351
phte1100 <phte1100@student.miun.se>
2024-04-23 20:44:06 +0200

    Nu kan jag logga in, registrera ny användare och komma åt en skyddad sida

--- server.js
+++ server.js
@@ -1,16 +1,46 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require('cors');
 const authRoutes = require("./routes/authRoutes");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.use(bodyParser.json());
+app.use(cors());
 
 // routes
-app.use("/api", authRoutes)
+app.use("/api", authRoutes);
 
-// start application
+// Protected routes
+app.get("/api/protected", authenticateToken, (req, res) => {
+    res.json({ message: "skyddad route! "});
+});
+
+// Validate token
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    console.log(`Authorization Header: ${authHeader}`);
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (token == null) {
+        return res.status(401).json({ message: "Not authorized for this route! - token missing!" });
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
+        if (err) {
+            return res.status(403).json({ message: "Not correct JWT" });
+        }
+
+        console.log('User from token:', JSON.stringify(user));
+        req.user = user;
+        next();
+    });
+}
+
+
+// Start application
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
